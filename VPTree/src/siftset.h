87b70d978c293a6ea25fa175165f4c584d7e65f5
remi.paucher <remi.paucher@student.ecp.fr>
2009-02-06 08:52:07 +0000

    Modification du code de SiftSet : chengement des int * en std::vector<int> Implémentation du K-Means sur l'espace des SIFTs

--- VPTree/src/siftset.h
+++ VPTree/src/siftset.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include <vector>
 #include <fstream>
@@ -5,12 +7,15 @@
 using namespace std;
 
 
+#define SAMPLE_LENGTH_FOR_K_MEANS 154
+
+
 /**
  *	Classe pour manipuler facilement les points SIFT
  *  (pour ne plus avoir à gérer les accès disque)
 
  	//Utilisation de la classe SiftSet
-	for (int *sift = siftset.begin(); sift != 0; sift = siftset.next())
+	for (std::vector<double> sift = siftset.begin(); sift != 0; sift = siftset.next())
 	{
 		//Do stuff with sift
 		delete sift;
@@ -21,13 +26,29 @@ class SiftSet
 {
 public:
 	SiftSet(char *name_database, char *name_sifts_files);
+
+	//Pour l'initialisation
 	void archive_files();
+	void compte_lignes();
 
-	int *get_descriptor(double index) const;
-	int *operator()(double index);
+	int getnbsift() {return nb_sifts;};
+	double get_distance(std::vector<double> sift1, std::vector<double> sift2);
+	void ajoute(std::vector<double> &a_modif, std::vector<double> &correction);
+	void divise(std::vector<double> &a_modif, double n);
 
-	int *begin();
-	int *next();
+	template <typename T>
+	void reset(std::vector<T> &a_modif);
+
+	//Pour obtenir un SIFT à partir de son index
+	std::vector<double> get_descriptor(double index) const;
+	std::vector<double> operator()(double index);
+
+	//Pour itérer sur tous les sifts
+	std::vector<double> begin();
+	std::vector<double> next();
+
+	//Pour faire un K-Means sur l'espace des SIFTs
+	void do_k_means(int k, std::vector<double> *centers);
 
 	
 
@@ -42,6 +63,8 @@ private:
 
 	double current_sift_index;				//Index du sift courant
 	double index_file;						//Index in files_names vector
+
+	int nb_sifts;							//Nombre de points SITFs total
 
 	
 
