2da915a0387ff6e71626f86384cf49d708467e79
hellwoxx <hellwoxx@a27398a2-dc2a-11dd-8561-ef618b9164eb>
2009-02-06 21:26:08 +0000

    Avec les fichiers Bof c'est mieux...^^

---
+++ trunk/VPTree/src/bof.cpp
@@ -0,0 +1,22 @@
+#include "bof.h"
+
+
+std::vector<double> Bof::get_kmeans_proba(std::vector<Feature> k_centers){
+    std::vector<double> proba,proba_j;
+    // Initialisation
+    for (unsigned int i=0; i<k_centers.size();i++){
+        proba[i]=0;
+    }
+    // Ajout des proba pour chaque feature du bag of feature. Le tout est divisé par le nombre de features.
+    for (unsigned int j=0; j<features.size(); j++){
+        proba_j=features[j].get_kmeans_proba(k_centers);
+        for (unsigned int i=0; i<k_centers.size();i++){
+            proba[i]+=proba_j[i]/features.size();
+        }
+
+    }
+
+    return proba;
+
+
+}
