faef77baeadcd1fa5c5bb3e5c20791b18f2a05bf
remi.paucher@student.ecp.fr <remi.paucher@student.ecp.fr@a27398a2-dc2a-11dd-8561-ef618b9164eb>
2009-02-07 19:58:50 +0000

    oubli des fichiers de la classe Vector...

---
+++ trunk/VPTree/src/vector.cpp
@@ -0,0 +1,61 @@
+#include "vector.h"
+
+
+/**
+ * Calcule la distance entre deux features
+ **/
+double Vector::get_distance_with(Vector &other)
+{
+	if (type == SIFT && other.type==SIFT)
+	{
+		double dist = 0;;
+		for (unsigned int i=0; i<this->size(); ++i)
+		{
+			dist += ((*this)[i] - other[i])*((*this)[i] - other[i]);
+		}
+		return dist;
+	}
+	else
+		exit(3);
+}
+
+
+/**
+ * Ajoute un feature au feature courant
+ **/
+void Vector::add_feature(Vector &other)
+{
+    if (type==SIFT && other.type==SIFT){
+        for (unsigned int i=0; i<this->size(); ++i)
+        {
+            (*this)[i] += other[i];
+        }
+    }
+    else {
+        exit(3);
+    }
+
+}
+
+/**
+ * Divise un feature par un double
+ **/
+void Vector::get_mul_cst(double factor)
+{
+	for (unsigned int i=0; i<this->size(); ++i)
+	{
+		(*this)[i] *= factor;
+	}
+}
+
+/**
+* Met à 0 tous les coeffs du feature
+**/
+void Vector::reset()
+{
+	for (unsigned int i=0; i<this->size(); ++i)
+	{
+		(*this)[i] = 0;
+	}
+}
+
---
+++ trunk/VPTree/src/vector.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+using namespace std;
+
+enum {SIFT};
+
+
+/**
+ * Classe Vecteur:
+ *	Classe dérivant des std::vector<double> avec des méthodes pour faire la distance entre deux vecteurs,
+ *  la somme, la multiplication par un scalaire...
+ **/
+class Vector : public std::vector<double>
+{
+public:
+	Vector(int typ=SIFT) {type = SIFT;};
+
+	double get_distance_with(Vector &other);
+	void add_feature(Vector &other);
+	void get_mul_cst(double other);
+	void reset();
+
+	void operator*=(double index)
+	{this->get_mul_cst(index); }
+
+	void operator+=(Vector &other)
+	{this->add_feature(other); }
+
+	double operator-(Vector &other)
+	{ return this->get_distance_with(other);}
+
+	//A implémenter
+	double compute_median() {return 0;}
+	double compute_second_moment(double val) {return 0;} //(somme(xi - val)²)
+
+
+private:
+	int type;
+
+};
