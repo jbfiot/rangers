99a5ac6e1418293d92a7f03f8d53dbc12de7d340
regis.behmo <regis.behmo@a27398a2-dc2a-11dd-8561-ef618b9164eb>
2009-02-13 13:12:56 +0000

    Pas de namespace dans les fichiers headers !

--- VPTree/src/tools.h
+++ VPTree/src/tools.h
@@ -6,6 +6,10 @@
 #include <string>
 
 using namespace std;
+// Pas de namespace dans les fichiers header
+// Here is why you should never write using declarations in header files at all, not even in a namespace scope: 
+// The meaning of the using declaration may change depending on what other headers happen to be #included before it in any given module. 
+// http://www.gotw.ca/publications/migrating_to_namespaces.htm
 
 int *get_random_set_indexes(int k,int n);
 
